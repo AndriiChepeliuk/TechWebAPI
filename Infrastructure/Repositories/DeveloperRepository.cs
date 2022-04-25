e3f553fd8d9c1bb3b991e2ed5aa7c42f6f0ad674
Andrii <andrewchepeliuk@gmail.com>
2022-04-25 17:00:44 +0300

    Inherited and Extended the GenericRepository by IDeveloperRepository

---
+++ Infrastructure/Repositories/DeveloperRepository.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class DeveloperRepository : GenericRepository<Developer>, IDeveloperRepository
+    {
+        public DeveloperRepository(ApplicationContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Developer>> GetPopulerDevelopers(int count)
+        {
+            return await Task.Run(() => _context.Developers.OrderByDescending(d => d.Followers).Take(count).ToList());
+        }
+    }
+}
